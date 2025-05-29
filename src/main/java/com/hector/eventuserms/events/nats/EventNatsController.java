b5051bc4145f75b7ff32a3cda3c200d6ea209c30
Hector Fabio Romero Bocanegra <hector.f.romero28@gmail.com>
2025-05-29 16:25:54 -0500

    fix: add missing functions in EventNatsController

--- src/main/java/com/hector/eventuserms/events/nats/EventNatsController.java
+++ src/main/java/com/hector/eventuserms/events/nats/EventNatsController.java
@@ -1,5 +1,7 @@
 package com.hector.eventuserms.events.nats;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.context.event.EventListener;
@@ -7,10 +9,15 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.hector.eventuserms.common.annotations.NatsHandler;
 import com.hector.eventuserms.common.nats.NatsMessageProcessor;
 import com.hector.eventuserms.events.EventService;
+import com.hector.eventuserms.events.dtos.request.CreateEventRequestDto;
+import com.hector.eventuserms.events.dtos.response.CreateEventResponseDto;
+import com.hector.eventuserms.events.dtos.response.FindEventsResponseDto;
 import com.hector.eventuserms.events.dtos.response.FindOneEventResponseDto;
+import com.hector.eventuserms.events.dtos.response.FindUpcomingEventResponseDto;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,17 +34,81 @@ public class EventNatsController {
     }
 
     @NatsHandler
+    @EventListener(condition = "#e.subject == T(com.hector.eventuserms.events.nats.EventSubjects).GET_ALL_EVENTS")
+    public void handleGetEvents(NatsMessageEvent e) throws JsonMappingException, JsonProcessingException {
+        // 1. Use the service.
+        List<FindEventsResponseDto> events = eventService.find();
+
+        // 2. Send the data to NATS
+        this.natsMessageProcessor.sendResponse(e.msg, events);
+    }
+
+    @NatsHandler
+    @EventListener(condition = "#e.subject == T(com.hector.eventuserms.events.nats.EventSubjects).GET_UPCOMING_EVENTS")
+    public void handleGetUpcomingEvents(NatsMessageEvent e) throws JsonMappingException, JsonProcessingException {
+        // 1. Use the service.
+        List<FindUpcomingEventResponseDto> events = eventService.findUpcoming();
+
+        // 2. Send the data to NATS
+        this.natsMessageProcessor.sendResponse(e.msg, events);
+    }
+
+    @NatsHandler
+    @EventListener(condition = "#e.subject == T(com.hector.eventuserms.events.nats.EventSubjects).GET_UPCOMING_EVENTS_BY_DATE")
+    public void handleGetUpcomingEventsByDate(NatsMessageEvent e) throws JsonMappingException, JsonProcessingException {
+        // 1. Parse the data received from NATS.
+        Instant date = Instant.parse(this.natsMessageProcessor.extractDataFromJson(e.msg).asText());
+
+        log.info("ANTES DE SERVICIO");
+        // 2. Use the service to return upcoming events today.
+        var result = eventService.findUpcomingEventsByDate(date);
+
+        log.info("Salí del servicio");
+
+        // 3. Send the data to NATS
+        this.natsMessageProcessor.sendResponse(e.msg, result);
+    }
+
+    @NatsHandler
     @EventListener(condition = "#e.subject == T(com.hector.eventuserms.events.nats.EventSubjects).GET_ID")
     public void handleGetEvent(NatsMessageEvent e) throws JsonMappingException, JsonProcessingException {
         UUID id = UUID.fromString(natsMessageProcessor.extractDataFromJson(e.msg).asText());
-
-        System.out.println("BUSCANDING EVENTO");
-        log.info("MAMA GUEVO");
         // 2. Use the service.
         FindOneEventResponseDto event = eventService.findOne(id);
 
         // 3. Send the data to NATS
         this.natsMessageProcessor.sendResponse(e.msg, event);
+    }
 
+    @NatsHandler
+    @EventListener(condition = "#e.subject == T(com.hector.eventuserms.events.nats.EventSubjects).CREATE")
+    public void handleCreateEvent(NatsMessageEvent e) throws JsonMappingException, JsonProcessingException {
+        // 1. Parse the data received from NATS.
+        JsonNode payload = natsMessageProcessor.extractDataFromJson(e.msg);
+        CreateEventRequestDto createEventRequestDto = this.natsMessageProcessor.fromJsonTree(payload,
+                CreateEventRequestDto.class);
+
+        // 2. Use the service.
+        CreateEventResponseDto newEvent = eventService.create(createEventRequestDto);
+
+        // * Note If we use `this.natsMessageProcessor.sendResponse(msg, newEvent);`
+        /*
+         * NATS will publish the data, but the NestJS client won't be able to receive
+         * it, due to the
+         * payload is not a valid "string" or "Uint8Arrays".
+         * 
+         * See: https://nats-io.github.io/nats.js/core/index.html#publish-and-subscribe
+         * 
+         * For that reason, we need to convert the class object into a String or send it
+         * inside a List, like:
+         * `this.natsMessageProcessor.sendResponse(msg, Arrays.asList(newEvent))` ->
+         * This
+         * works
+         * 
+         * 
+         * Honestly... I’m impressed this is necessary. LMAO
+         */
+        // 3. Send the data to NATS
+        this.natsMessageProcessor.sendResponse(e.msg, newEvent);
     }
 }
--- src/main/java/com/hector/eventuserms/events/nats/EventPublisher.java
+++ src/main/java/com/hector/eventuserms/events/nats/EventPublisher.java
@@ -11,55 +11,55 @@ import jakarta.annotation.PostConstruct;
 @Component
 public class EventPublisher {
 
-    private final Connection natsConnection;
-    private final ApplicationEventPublisher eventPublisher;
+        private final Connection natsConnection;
+        private final ApplicationEventPublisher eventPublisher;
 
-    public EventPublisher(Connection natsConnection, ApplicationEventPublisher eventPublisher) {
-        this.natsConnection = natsConnection;
-        this.eventPublisher = eventPublisher;
-    }
+        public EventPublisher(Connection natsConnection, ApplicationEventPublisher eventPublisher) {
+                this.natsConnection = natsConnection;
+                this.eventPublisher = eventPublisher;
+        }
 
-    /*
-     * If we want to use AOP to intercept errors that may occur during
-     * business logic or serialization while handling NATS messages,
-     * we can't use callbacks directly, as they bypass the Spring proxy.
-     *
-     * Instead, we publish Spring custom events here,
-     * allowing methods annotated with @EventListener and @NatsHandler
-     * to be intercepted by aspects that centralize error handling.
-     *
-     * The logic is split into three components:
-     * - EventPublisher: publishes the events.
-     * - EventNatsController: listens to the events and executes logic.
-     * - EventSubjects: holds the NATS subject names (we avoid enums for easier
-     * conditional arg used in @EventListener).
-     */
-    @PostConstruct
-    public void initialize() {
-        Dispatcher dispatcher = natsConnection.createDispatcher();
+        /*
+         * If we want to use AOP to intercept errors that may occur during
+         * business logic or serialization while handling NATS messages,
+         * we can't use callbacks directly, as they bypass the Spring proxy.
+         *
+         * Instead, we publish Spring custom events here,
+         * allowing methods annotated with @EventListener and @NatsHandler
+         * to be intercepted by aspects that centralize error handling.
+         *
+         * The logic is split into three components:
+         * - EventPublisher: publishes the events.
+         * - EventNatsController: listens to the events and executes logic.
+         * - EventSubjects: holds the NATS subject names (we avoid enums for easier
+         * conditional arg used in @EventListener).
+         */
+        @PostConstruct
+        public void initialize() {
+                Dispatcher dispatcher = natsConnection.createDispatcher();
 
-        // Register each NATS subject and publish the corresponding event
-        dispatcher.subscribe(EventSubjects.GET_ID,
-                (msg) -> this.publishNatsEvent(msg, EventSubjects.GET_ID));
+                // Register each NATS subject and publish the corresponding event
+                dispatcher.subscribe(EventSubjects.GET_ID,
+                                (msg) -> this.publishNatsEvent(msg, EventSubjects.GET_ID));
 
-        dispatcher.subscribe(EventSubjects.GET_ALL_EVENTS,
-                (msg) -> this.publishNatsEvent(msg, EventSubjects.GET_ALL_EVENTS));
+                dispatcher.subscribe(EventSubjects.GET_ALL_EVENTS,
+                                (msg) -> this.publishNatsEvent(msg, EventSubjects.GET_ALL_EVENTS));
 
-        dispatcher.subscribe(EventSubjects.GET_UPCOMING_EVENTS,
-                (msg) -> this.publishNatsEvent(msg, EventSubjects.GET_UPCOMING_EVENTS));
+                dispatcher.subscribe(EventSubjects.GET_UPCOMING_EVENTS,
+                                (msg) -> this.publishNatsEvent(msg, EventSubjects.GET_UPCOMING_EVENTS));
 
-        dispatcher.subscribe(EventSubjects.GET_UPCOMING_EVENTS_BY_DATE,
-                (msg) -> this.publishNatsEvent(msg, EventSubjects.GET_UPCOMING_EVENTS_BY_DATE));
+                dispatcher.subscribe(EventSubjects.GET_UPCOMING_EVENTS_BY_DATE,
+                                (msg) -> this.publishNatsEvent(msg, EventSubjects.GET_UPCOMING_EVENTS_BY_DATE));
 
-        dispatcher.subscribe(EventSubjects.CREATE,
-                (msg) -> this.publishNatsEvent(msg, EventSubjects.CREATE));
+                dispatcher.subscribe(EventSubjects.CREATE,
+                                (msg) -> this.publishNatsEvent(msg, EventSubjects.CREATE));
 
-    }
+        }
 
-    // Creates a custom event with the received NATS message and publishes it using
-    // Spring's event publisher.
-    private void publishNatsEvent(Message msg, String subject) {
-        NatsMessageEvent newNatsMessage = new NatsMessageEvent(msg, subject);
-        this.eventPublisher.publishEvent(newNatsMessage);
-    }
+        // Creates a custom event with the received NATS message and publishes it using
+        // Spring's event publisher.
+        private void publishNatsEvent(Message msg, String subject) {
+                NatsMessageEvent newNatsMessage = new NatsMessageEvent(msg, subject);
+                this.eventPublisher.publishEvent(newNatsMessage);
+        }
 }
